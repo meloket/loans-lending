cd3ccae68deab6fe510f18864eefc97a6a9b3c9f
Clover Dev <cloverdev88@gmail.com>
2021-10-07 08:27:38 -0400

    Initial commit

---
+++ contracts/Loans.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity >=0.4.22 <0.9.0;
+
+contract Loans {
+  constructor() public {
+  }
+
+  function offerLoan() public {
+
+  }
+
+  function requestLoan() public {
+
+  }
+
+  function acceptOffer() public {
+
+  }
+
+  function fundLoan() public {
+  
+  } 
+}
---
+++ test/loans.js
@@ -0,0 +1,13 @@
+const Loans = artifacts.require("Loans");
+
+/*
+ * uncomment accounts to access the test accounts made available by the
+ * Ethereum client
+ * See docs: https://www.trufflesuite.com/docs/truffle/testing/writing-tests-in-javascript
+ */
+contract("Loans", function (/* accounts */) {
+  it("should assert true", async function () {
+    await Loans.deployed();
+    return assert.isTrue(true);
+  });
+});
